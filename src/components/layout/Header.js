c1fe82c6be7a4af937482e0601ce462da20ba747
Tarun <97669140+XENO2410@users.noreply.github.com>
2024-05-03 20:03:32 +0530

    check commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,7 +7,7 @@ export default function Header(){
 
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
         <Link className="text-primary font-semibold text-2xl"  href={'/'}>
-          Sip & Bite
+          AMUL NSUT
         </Link>
           <Link href={'/'}>Home</Link>
           <Link href={''}>Menu</Link>
